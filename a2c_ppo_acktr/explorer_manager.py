791c573565122810791982f899580abd077c925a
Zafarali Ahmed <zafarali@gra-login2.graham.sharcnet>
2019-03-28 13:16:36 -0400

    Exploration manager updated to clip mu.

--- a2c_ppo_acktr/explorer_manager.py
+++ a2c_ppo_acktr/explorer_manager.py
@@ -5,7 +5,7 @@ Manages the setting and updating of the exploration parameter.
 import numpy as np
 
 class GaussianExplorer(object):
-    def __init__(self, mu=0.5, upper_bound=1.0, lower_bound=0.0, spread=0.5):
+    def __init__(self, mu=0.5, upper_bound=0.8, lower_bound=0.1, spread=0.5):
         self.mu = mu
         self.spread = spread
         self.upper_bound = upper_bound
@@ -22,5 +22,7 @@ class GaussianExplorer(object):
         rewards_energy_dist = np.exp(rewards)
         rewards_energy_dist /= np.sum(np.exp(rewards))
 
-        self.mu = np.sum(exp_ps * rewards_energy_dist)
+        self.mu = np.clip(
+                np.sum(exp_ps * rewards_energy_dist),
+                self.lower_bound, self.upper_bound)
 
